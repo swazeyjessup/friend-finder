3ba922b633775d9b9c3626149a23b55917d832c1
Swazey Jessup <swazey_jessup@hotmail.com>
2020-03-24 00:03:52 -0600

    fixed bugs

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+	array: [
+		{
+			name: 'Joe',
+			photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+			scores: ['1','1','5','1','1','1','1','1','1','1']
+		}
+	]
+}
